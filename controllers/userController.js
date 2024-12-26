7e42a21fa0eeec9f84e492aa5393734b62f84d32
naurazhaf <naurazhaf12@gmail.com>
2024-12-26 16:52:35 +0700

    add userinfo

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,16 +4,22 @@ const { message } = require("../validators/registerUser.validator");
 
 const getUserInfo = async (req, res) => {
   try {
-    const { rows } = await pool.query("SELECT id, username FROM users WHERE id = $1", [
-      req.user.id,
-    
-    ]);
+    const { rows } = await pool.query(
+      "SELECT username, wincount FROM users WHERE id = $1",
+      [req.user.id]
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
     res.status(200).json(rows[0]);
   } catch (error) {
-    console.error("Error fetching user info:", error);
+    console.error("Error fetching user info:", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
 
 const getLeaderboard = async (req, res) => {
   try {
